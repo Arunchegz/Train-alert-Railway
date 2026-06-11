faf69da664891fbe66efe7319f34573f47138854
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-11 18:08:36 +0530

    Update models.py

--- models.py
+++ models.py
@@ -23,6 +23,9 @@ if DATABASE_URL.startswith("postgres://"):
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
 )
 
 metadata = MetaData()
